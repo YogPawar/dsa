012a558fc39c39003a218cb785c66786a6add1d7
Yogesh Pawar <pawaryog03@gmail.com>
2024-02-18 21:41:45 +0530

    Optimized bubble sort if array is already sorted

--- dsa-leetcode-practice/src/com/misc/sorting/bubble/BubbleSort.java
+++ dsa-leetcode-practice/src/com/misc/sorting/bubble/BubbleSort.java
@@ -20,10 +20,40 @@ public class BubbleSort {
     }
   }
 
+  public static void swapOptimize(int[] input, int i, int j) {
+    int temp = input[i];
+    input[i] = input[j];
+    input[j] = temp;
+  }
+
   public static void main(String[] args) {
     int[] input = {4, 6, 2, 1, 8, 3};
-    sort(input);
-    System.out.print(Arrays.toString(input));
+    int[] input1 = {1, 2, 3, 4, 5, 6};
+    bubbleSort(input1);
+    System.out.print(Arrays.toString(input1));
+  }
+
+  public static void bubbleSort(int[] input) {
+    int n = input.length - 1;
+    for (int i = 0; i < n; i++) {
+      boolean swap = false;
+      for (int j = 0; j < n - i; j++) {
+        if (input[j] > input[j + 1]) {
+          swap = true;
+          optimizeSwap(input, j);
+        }
+      }
+      if (!swap) {
+        System.out.println("Array is already sorted");
+        break;
+      }
+    }
+  }
+
+  private static void optimizeSwap(int[] input, int j) {
+    int temp = input[j];
+    input[j] = input[j + 1];
+    input[j + 1] = temp;
   }
 }
 

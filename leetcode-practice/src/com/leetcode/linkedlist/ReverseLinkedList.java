2d209470efc4b00fcf25846ff0e6392df0c9964a
Yogesh Pawar <pawaryog03@gmail.com>
2024-03-31 10:32:04 +0530

    Merge alternative word in string

---
+++ leetcode-practice/src/com/leetcode/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,17 @@
+package com.leetcode.linkedlist;
+
+public class ReverseLinkedList {
+
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode current = head;
+    while (current != null) {
+      ListNode temp = current.next;
+      current.next = prev;
+      prev = temp;
+      current = temp;
+    }
+    return prev;
+  }
+
+}

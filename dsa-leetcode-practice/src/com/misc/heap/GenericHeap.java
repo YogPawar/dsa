7ee547c0240492ec67d8299ffc31ad31b0f211d7
Yogesh Pawar <pawaryog03@gmail.com>
2024-04-12 21:10:34 +0530

    added remove method to heap

--- dsa-leetcode-practice/src/com/misc/heap/GenericHeap.java
+++ dsa-leetcode-practice/src/com/misc/heap/GenericHeap.java
@@ -8,6 +8,20 @@ public class GenericHeap {
 
   private final ArrayList<Integer> heap = new ArrayList<>();
 
+  public static void main(String[] args) {
+    GenericHeap heap = new GenericHeap();
+    heap.insert(10);
+    heap.insert(4);
+    heap.insert(2);
+    heap.insert(25);
+    heap.insert(100);
+    heap.showHeap();
+    heap.remove();
+    heap.showHeap();
+    heap.remove();
+    heap.showHeap();
+  }
+
   private int parent(int childIndex) {
     return childIndex / 2;
   }
@@ -44,14 +58,39 @@ public class GenericHeap {
     System.out.println(heap);
   }
 
-  public static void main(String[] args) {
-    GenericHeap heap = new GenericHeap();
-    heap.insert(10);
-    heap.insert(4);
-    heap.insert(2);
-    heap.insert(25);
-    heap.insert(100);
-    heap.showHeap();
+  public Integer remove() {
+    if (heap.size() == 0) {
+      return 0;
+    }
+    if (heap.size() == 1) {
+      return heap.remove(0);
+    }
+    int maxValue = heap.get(0);
+    heap.set(0, heap.remove(heap.size() - 1));
+    sinkdown(0);
+    return maxValue;
+  }
+
+  private void sinkdown(int index) {
+    int maxIndex = index;
+
+    while (true) {
+      int leftIndex = leftChild(index);
+      int rightIndex = rightChild(index);
+      if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+        maxIndex = leftIndex;
+      }
+
+      if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+        maxIndex = rightIndex;
+      }
+      if (maxIndex != index) {
+        swap(index, maxIndex);
+        index = maxIndex;
+      } else {
+        return;
+      }
+    }
   }
 }
 

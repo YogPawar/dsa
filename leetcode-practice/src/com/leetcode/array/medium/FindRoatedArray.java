901b408bf2d073cc38d8b9d8fb6bab8aed93c45a
Yogesh Pawar <pawaryog03@gmail.com>
2024-07-15 07:59:17 +0530

    Added code to find the element into roated array

--- leetcode-practice/src/com/leetcode/array/medium/FindRoatedArray.java
+++ leetcode-practice/src/com/leetcode/array/medium/FindRoatedArray.java
@@ -4,11 +4,46 @@ public class FindRoatedArray {
 
   public static void main(String[] args) {
     //[4,5,6,7,0,1,2]
+    System.out.println("Use Case 1: [4,5,6,7,0,1,2] target : 0");
+    int[] nums = {4, 5, 6, 0, 1, 2};
+    System.out.println("Element found at index : " + search(nums, 0));
+
+    System.out.println("Use Case 2: [6,7,8,9,10,11] target : 10");
+    int[] nums1 = {6, 7, 8, 9, 10, 11};
+    System.out.println("Element found at index : " + search(nums1, 10));
+
+    System.out.println("Use Case 3: [11,10,9,8,7,6] target : 10");
+    int[] nums2 = {11, 10, 9, 8, 7, 6};
+    System.out.println("Element found at index : " + search(nums2, 10));
   }
 
-  public int search(int[] nums, int target) {
-    int mid = nums.length / 2; // 8/2 = 4
+  private static int search(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length - 1; //5
+    while (low <= high) {
+      //calculate the mid
+      int mid = (low + high) / 2;
 
+      //target found at nums[mid] then return the index
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      if (nums[low] <= nums[mid]) {
+        if (nums[low] <= target && target < nums[mid]) {
+          //rest the high to mid -1
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      } else {
+        if (nums[mid] > target && target <= nums[high]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      }
+    }
     return -1;
   }
 }

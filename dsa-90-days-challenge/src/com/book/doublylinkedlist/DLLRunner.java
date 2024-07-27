07e9c2f14804d2db7da8635ce1fbde8f929f3030
Yogesh Pawar <pawaryog03@gmail.com>
2024-07-27 11:09:22 +0530

    Completed insertion operations in DLL

--- dsa-90-days-challenge/src/com/book/doublylinkedlist/DLLRunner.java
+++ dsa-90-days-challenge/src/com/book/doublylinkedlist/DLLRunner.java
@@ -4,9 +4,16 @@ public class DLLRunner {
 
   public static void main(String[] args) {
     DoublyLinkedList dll = new DoublyLinkedList(5);
-    dll.insertAtBegin(6);
-
+    System.out.println("Head : " + dll.getHead());
+    System.out.println("Size : " + dll.getSize());
+    dll.insertAtEnd(6);
+    dll.insertAtEnd(7);
+    dll.insertAtEnd(8);
+    dll.insertAtEnd(9);
+    System.out.println("Head : " + dll.getHead());
+    System.out.println("Size : " + dll.getSize());
     dll.printDLL();
+    System.out.println();
   }
 
 }
--- dsa-90-days-challenge/src/com/book/doublylinkedlist/DoublyLinkedList.java
+++ dsa-90-days-challenge/src/com/book/doublylinkedlist/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package com.book.doublylinkedlist;
 
+import org.w3c.dom.Node;
+
 public class DoublyLinkedList {
 
   private DLLNode head;
@@ -21,11 +23,44 @@ public class DoublyLinkedList {
     size++;
   }
 
+  public void insertAtEnd(int data) {
+    DLLNode newNode = new DLLNode(data);
+    if (head == null) {
+      head = newNode;
+    } else {
+      //traverse the list till end
+      DLLNode temp = head;
+      while (temp.getNext() != null) {
+        temp = temp.getNext();
+      }
+
+      newNode.setPrev(temp);
+      temp.setNext(newNode);
+    }
+    size++;
+  }
+
   public void printDLL() {
     DLLNode temp = head;
     while (temp != null) {
       System.out.print(temp.getData() + " ");
       temp = temp.getNext();
     }
+  }
+
+  public int getHead() {
+    return head.getData();
+  }
+
+  public void setHead(DLLNode head) {
+    this.head = head;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
   }
 }

a61ec999d4f4fca51d9f84455044a547452ae12d
Yogesh Pawar <pawaryog03@gmail.com>
2024-07-29 08:07:11 +0530

    Added code for removing the last node from circular linked list

--- dsa-90-days-challenge/src/com/book/circularlinkedlist/CLLRunner.java
+++ dsa-90-days-challenge/src/com/book/circularlinkedlist/CLLRunner.java
@@ -5,11 +5,16 @@ public class CLLRunner {
   public static void main(String[] args) {
     CircularLinkedList cll = new CircularLinkedList();
     cll.add(10);
-    cll.add(12);
-    cll.add(13);
-
-    System.out.println("List : ");
+    cll.addToTail(5);
+    cll.addToTail(35);
+    cll.addToTail(25);
+    cll.addToTail(15);
     cll.printList();
+
+    System.out.println("\n Length : "+cll.getLength());
+    System.out.println(cll.removeLast());
+    cll.printList();
+    System.out.println("\n Length : " + cll.getLength());
   }
 
 }
--- dsa-90-days-challenge/src/com/book/circularlinkedlist/CircularLinkedList.java
+++ dsa-90-days-challenge/src/com/book/circularlinkedlist/CircularLinkedList.java
@@ -1,5 +1,6 @@
 package com.book.circularlinkedlist;
 
+import javax.print.DocFlavor.READER;
 import org.w3c.dom.Node;
 
 public class CircularLinkedList {
@@ -18,7 +19,6 @@ public class CircularLinkedList {
   private void addToHead(int data) {
     CLLNode newNode = new CLLNode(data);
     newNode.setNext(head);
-
     if (head != null) {
       CLLNode temp = head;
       while (temp.getNext() != head) {
@@ -30,8 +30,6 @@ public class CircularLinkedList {
     }
     head = newNode;
     length++;
-
-
   }
 
   public void addToTail(int data) {
@@ -39,9 +37,52 @@ public class CircularLinkedList {
     head = head.getNext();
   }
 
+  public int removeFirst() {
+    if (head == null) {
+      return 0;
+    } else {
+      CLLNode tail = head;
+      CLLNode nodeToDelete = head;
+      //find the last element
+      while (tail.getNext() != head) {
+        tail = tail.getNext();
+      }
+      tail.setNext(nodeToDelete.getNext());
+      head = nodeToDelete.getNext();
+      nodeToDelete.setNext(null);
+      length--;
+      if (length <= 0) {
+        head = null;
+      }
+      return nodeToDelete.getData();
+    }
+  }
+
+  public int removeLast() {
+    CLLNode nodeToDelete = head;
+    CLLNode nodePTail = head;
+
+    if (nodeToDelete == nodeToDelete.getNext()) { //list having only one element
+      head = null;
+      length--;
+      return nodePTail.getData();
+    } else {
+      while (nodeToDelete.getNext() != head) {
+        if (nodeToDelete.getNext().getNext() == head) {
+          nodePTail = nodeToDelete;
+        }
+        nodeToDelete = nodeToDelete.getNext();
+      }
+      nodePTail.setNext(nodeToDelete.getNext());
+      nodeToDelete.setNext(null);
+      length--;
+    }
+    return nodeToDelete.getData();
+  }
+
   public void printList() {
     CLLNode traverse = head;
-    if (head != null) {
+    if (traverse != null) {
       do {
         System.out.print(traverse.getData() + " ");
         traverse = traverse.getNext();
@@ -49,7 +90,15 @@ public class CircularLinkedList {
     }
   }
 
-
+  public void printcll() {
+    int i = 0;
+    CLLNode traverse = head;
+    while (i < length) {
+      System.out.print(traverse.getData() + " ");
+      traverse = traverse.getNext();
+      i += 1;
+    }
+  }
 
   public int getLength() {
     return length;
